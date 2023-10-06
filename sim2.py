280807444b0e23cd81037debb872f2283a1fc291
freshtaste <liujizhou123@hotmail.com>
2023-10-06 11:29:50 -0400

    udpate

--- sim2.py
+++ sim2.py
@@ -218,7 +218,7 @@ def get_table6():
         with open("Table6_part1.txt", "a") as f:
             print(m, file=f)
         qk_pairs = [(q,k) for q in [1,2,4,6,9] for k in [1,2,3,4,5,6]]
-        result = {(q,k): reject_prob_parrell(covariates, k, q, 1280, tau=0, ntrials=1000, more=False, design='MT') for q, k in qk_pairs}
+        result = {(q,k): reject_prob_parrell(covariates, k, q, 1280, tau=0, ntrials=1000, more=False, design=m) for q, k in qk_pairs}
         results_null.append(result)
         for q in [1,2,4,6,9]:
             for k in [1,2,3,4,5,6]:
