2936222a4980c73197672721fdd4a3470c9ec2ba
freshtaste <liujizhou123@hotmail.com>
2023-10-06 10:21:06 -0400

    update

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import random
 random.seed(123)
 
 # 1. Get Table 3
-get_table3()
+#get_table3()
 
 # 2. Get Table 4
 #get_table4()
 
 # 3. Get Table 5
-#get_table5()
+get_table5()
 
 # 4. Get Table 6
 #get_table6()
